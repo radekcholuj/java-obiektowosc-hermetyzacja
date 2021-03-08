2c3b0cf24326ff6e005bd1913cf80464d967b241
Radoslaw Choluj <radekcholuj@wp.pl>
2021-03-08 21:25:44 +0100

    #3 poprawienie programu dotyczacego dodania wyswietlania wniosku

--- src/Comunication.java
+++ src/Comunication.java
@@ -10,15 +10,6 @@ public class Comunication {
         applications[numberOfAplications++] = application;
     }
 
-    public void findApplication(int number) {
-        for (int i = 0; i < applications.length && applications[i] != null; i++) {
-            if (number == applications[i].getAplicationNumber()) {
-                System.out.println("Status wniosku: " + applications[i].getStatus());
-            }
-        }
-
-    }
-
     public void displayAllApplications() {
         for (int i = 0; i < applications.length && applications[i] != null; i++) {
             System.out.println(applications[i].getAplicationNumber());
@@ -33,8 +24,9 @@ public class Comunication {
 
     public void displayApplicationStatus(int number) {
         for (int i = 0; i < applications.length && applications[i] != null; i++) {
-            if (i == number) {
-                System.out.println("Zmieniono status na: " + applications[number].getStatus());
+            if (applications[i].getAplicationNumber() == number) {
+                System.out.println("Status wniosku: " + applications[i].getStatus());
+                break;
             }
         }
     }
